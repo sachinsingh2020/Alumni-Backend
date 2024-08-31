1c88660e4444832a9d971218371a1235904b7133
sachin singh <sachinthegreat1001@gmail.com>
2024-08-31 19:54:09 +0530

    added multiple origins

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ config({
 
 const app = express();
 
-// middleware 
+// Middleware
 app.use(express.json());
 app.use(
     express.urlencoded({
@@ -19,13 +19,24 @@ app.use(
 );
 
 app.use(cookieParser());
+
+// Setup CORS to handle multiple origins
+const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:3000"];
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: function (origin, callback) {
+        // Allow requests with no origin (e.g., mobile apps, curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
-// importing and using routes 
+// Importing and using routes 
 import user from "./routes/userRoutes.js"
 // import payment from "./routes/paymentRoutes.js"
 import alumni from "./routes/alumniRoutes.js"
@@ -36,7 +47,7 @@ app.use('/api/v1', user);
 app.use('/api/v1', alumni);
 app.use('/api/v1', jobPortal);
 
-// printing server is working 
+// Printing server is working 
 app.get('/', (req, res) => {
     res.send('Server is working');
 })
